e9b9e7e813a47e17bfdb3b6b721f0504c6f64d47
islamic-content1 <s12216944@stu.najah.edu>
2026-08-12 23:24:03 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_VERSION = "ibadah-2026.08.12.3";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_VERSION).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_VERSION ? caches.delete(key) : null)))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("message", event => {
+  if (event.data && event.data.type === "SKIP_WAITING") self.skipWaiting();
+});
+
+self.addEventListener("fetch", event => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  if (req.mode === "navigate") {
+    event.respondWith(
+      fetch(req, {cache: "no-store"})
+        .then(res => {
+          const copy = res.clone();
+          caches.open(CACHE_VERSION).then(cache => cache.put("./index.html", copy));
+          return res;
+        })
+        .catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE_VERSION).then(cache => cache.put(req, copy));
+      return res;
+    }))
+  );
+});
